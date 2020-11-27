cd6f7577e870d4786ac6eb0607a0dc8bcc138637
ses1142000 <75112162+ses1142000@users.noreply.github.com>
2020-11-27 12:52:32 +0530

    Add files via upload

---
+++ day 2 py internship.py
@@ -0,0 +1,18 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> (a,b,c)=(3,4,5)
+>>> print(a/10)
+0.3
+>>> print(b*50)
+200
+>>> print(c+60)
+65
+>>> str="sharon,sam,saji,sindhu,philip"
+>>> print(str.replace("saji","g"))
+sharon,sam,g,sindhu,philip
+>>> (a,b)=(5,6.55)
+>>> print(float(a))
+5.0
+>>> print(int(b))
+6
+>>> 
