ae79ef8aa113c95a6a774e281b5c57c61a348e97
ses1142000 <75112162+ses1142000@users.noreply.github.com>
2020-11-27 18:08:22 +0530

    Add files via upload

---
+++ day 5 py internship.py
@@ -0,0 +1,61 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def addition(a,b):
+	add=a+b
+	print("addition of two numbers:",a+b)
+
+	
+>>> addition(20,30)
+addition of two numbers: 50
+>>> 
+>>> 
+>>> def subraction(a,b):
+	sub=a-b
+	print("subraction of two numbers:",a-b)
+
+	
+>>> subraction(30,20)
+subraction of two numbers 10
+>>> 
+>>> 
+>>> def division(a,b):
+	div=a/b
+	print("division of two numbers:",a/b)
+
+	
+>>> division(20,30)
+division of two numbers: 0.6666666666666666
+>>> 
+>>> 
+>>> def multiplication(a,b):
+	mul=a*b
+	print("multiplication of two numbers:",a*b)
+
+	
+>>> multiplication(20,30)
+multiplication of two numbers: 600
+>>> 
+>>> 
+>>> def covid(patient_name,body_temperature):
+	print("covid statement:"+patient_name+","+body_temperature)
+
+	
+>>> covid('saranya','110 degree')
+covid statement:saranya,110 degree
+>>> 
+>>> 
+>>> def covid(patient_name):
+	print("name of the patient:"+patient_name+"")
+
+	
+>>> covid('saranya')
+name of the patient:saranya
+>>> 
+>>> 
+>>> def covid(body_temperature):
+	print("body temperature of the patient:"+body_temperature+"")
+
+	
+>>> covid('110 degrees')
+body temperature of the patient:110 degrees
+>>> 
